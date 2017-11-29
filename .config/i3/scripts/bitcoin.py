124bcd08eb685ddf37e5808643808ac9d23f86ca
Jacob Potter <jmp1617@g.rit.edu>
2017-11-29 12:32:26 +0000

    updated theme: red black white

--- .config/i3/scripts/bitcoin.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-
-import requests
-
-print(requests.get(url='https://api.coindesk.com/v1/bpi/currentprice/usd.json').json()['bpi']['USD']['rate'].split('.')[0],"")
--- .config/i3/scripts/fan.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-
-import subprocess
-
-print((str(subprocess.check_output(["sensors", "-Au"])).split("fan1_input: "))[1].split('.')[0],"rpm ")
